2efb3f47e0a083771228f2ef11e08dcf63d48226
robzor92 <robin.eric.andersson@gmail.com>
2022-06-16 21:22:15 +0200

    fix

--- local_setup.sh
+++ local_setup.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+set -e
 
-curl -LO https://repo.anaconda.com/miniconda/Miniconda3-py38_4.8.3-Linux-x86_64.sh
+if [ "$(uname)" == "Darwin" ]; then
+    curl -L https://repo.anaconda.com/miniconda/Miniconda3-py38_4.8.3-MacOSX-x86_64.sh -o Miniconda3-py38_4.8.3-x86_64.sh
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    curl -L https://repo.anaconda.com/miniconda/Miniconda3-py38_4.8.3-Linux-x86_64.sh -o Miniconda3-py38_4.8.3-x86_64.sh
+fi
 
-bash Miniconda3-py38_4.8.3-Linux-x86_64.sh -p miniconda3 -b
+chmod u+x ./Miniconda3-py38_4.8.3-x86_64.sh && ./Miniconda3-py38_4.8.3-x86_64.sh -p miniconda -b && rm Miniconda3-py38_4.8.3-x86_64.sh
 
-./miniconda3/bin/conda install -y --no-deps pycryptosat libcryptominisat
-./miniconda3/bin/conda config --set sat_solver pycryptosat
-./miniconda3/bin/conda update conda -y -q
-./miniconda3/bin/conda create --prefix ./miniconda3/envs/hopsworks python=3.8 -y
+./miniconda/bin/conda install -y --no-deps pycryptosat libcryptominisat
+./miniconda/bin/conda config --set sat_solver pycryptosat
+./miniconda/bin/conda update conda -y -q
+./miniconda/bin/conda create --prefix ./miniconda/envs/hopsworks python=3.8 -y
 
-./miniconda3/envs/hopsworks/bin/pip install -U jupyterlab==2.2.8 'git+https://github.com/logicalclocks/feature-store-api@master#egg=hsfs[python]&subdirectory=python' 'git+https://github.com/logicalclocks/hopsworks-api@main#egg=hopsworks&subdirectory=python' --force-reinstall
+./miniconda/envs/hopsworks/bin/pip install jupyterlab==2.2.8 'git+https://github.com/logicalclocks/feature-store-api@master#egg=hsfs[python]&subdirectory=python' 'git+https://github.com/logicalclocks/hopsworks-api@main#egg=hopsworks&subdirectory=python' --force-reinstall
 
-./miniconda3/envs/hopsworks/bin/jupyter lab
-
-
+./miniconda/envs/hopsworks/bin/jupyter trust quickstart.ipynb
+./miniconda/envs/hopsworks/bin/jupyter lab
