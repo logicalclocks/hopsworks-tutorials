cfcba38d1583de786dd0aba92cc1415eebbfc093
Roman <r4bckagarl@gmail.com>
2022-08-21 22:39:15 +0200

    testing gh actions #12

--- scripts/run-nyc-data-ingestion-pipelines.sh
+++ scripts/run-nyc-data-ingestion-pipelines.sh
@@ -6,9 +6,6 @@ echo "$PWD"
 
 cd advanced_tutorials/nyc_taxi_fares/python_files
 
-sudo apt-get install python-dev python-pip libxml2-dev libxslt1-dev zlib1g-dev libffi-dev libssl-dev
-
-python3-pip install secrets
 echo "New rides data ingestion pipeline starts"
 python3 1.1_rides_fg.py
 echo "New fares data ingestion pipeline starts"
