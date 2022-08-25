bc7a836710ba1e67e75a4e8178f23ce8abd8bc87
Maksym Zhytnikov <63515947+Maxxx-zh@users.noreply.github.com>
2022-08-25 20:36:33 +0300

    Added Model Prediction pipeline

--- scripts/data_flow.sh
+++ scripts/data_flow.sh
@@ -12,3 +12,6 @@ python3 1_calendar_fg.py
 
 echo "New Electricity data ingestion pipeline starts"
 python3 1_electricity_fg.py
+
+echo "Model Prediction pipeline starts"
+python3 2_model_prediction.py
