0f9a181c66aedcb4597c78bd2ba2af8605d1cbaf
Maksym Zhytnikov <63515947+Maxxx-zh@users.noreply.github.com>
2022-09-26 17:17:05 +0300

    Removed Heroku

--- bitcoin/Heroku/settings.py
+++
@@ -1,5 +0,0 @@
-PARSE_NEW_TWEETS =  1 # if 0 (aka False), only BTC timeseries data will be parsed
-                   # and processed. Twitter API will not be used.
-LOCALLY = 1 # if 0 (aka False), environmental vars will be NOT parsed from
-            # .env file in this dir but from Heroku environment itself.
-            # Use LOCALLY = 1 when you run streamlit_app.py locally.
--- bitcoin/Heroku/setup.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p ~/.streamlit/
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
