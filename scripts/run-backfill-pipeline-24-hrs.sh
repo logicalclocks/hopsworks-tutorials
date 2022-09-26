c4b41418ce6ba39ab8da444bae8b012963c435b7
davitbzh <44586065+davitbzh@users.noreply.github.com>
2022-09-26 23:33:29 +0200

    Electricity prices tutorial (#77)
    Electricity price prediction tutorial

---
+++ scripts/run-backfill-pipeline-24-hrs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "$PWD"
+
+set -e
+
+echo "Running feature backfill pipeline"
+cd advanced_tutorials/electricity 
+
+jupyter nbconvert --to notebook --execute 2_feature_pipeline.ipynb
