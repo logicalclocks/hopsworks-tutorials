a63f2317a72ec14d3cfdfea4972cb6e548ae3bc3
Roman <58133204+rktraz@users.noreply.github.com>
2022-11-02 00:11:36 +0100

    complete tutorial, but without README

--- advanced_tutorials/citibike/functions.py
+++ advanced_tutorials/citibike/functions.py
@@ -1,5 +1,6 @@
 import math
 import requests
+import joblib
 from datetime import datetime
 import zipfile
 from io import BytesIO
@@ -15,16 +16,30 @@ warnings.filterwarnings("ignore")
 from dotenv import load_dotenv
 load_dotenv()
 
+
+def select_stations_info(df):
+    df_res = df[["start_station_id", "start_station_name",
+                 "start_lat", "start_lng"]].rename(
+                                                  columns={
+                                                  "start_station_id": "station_id",
+                                                  "start_station_name": "station_name",
+                                                  "start_lat": "lat",
+                                                  "start_lng": "long"}
+                                                  )
+    df_res = df_res.drop_duplicates()
+    return df_res
+
+
 def process_df(original_df):
-    res = original_df[["started_at", "start_station_id"]]
-    res.started_at = pd.to_datetime(res.started_at)
-    res.started_at = res.started_at.dt.floor('d')
-    res = res.groupby(["started_at",
-                       "start_station_id"]).value_counts().reset_index()
-    res = res.rename(columns={"started_at": "date",
-                              "start_station_id": "station_id",
-                              0: "users_count"})
-    return res
+    df_res = original_df[["started_at", "start_station_id"]]
+    df_res.started_at = pd.to_datetime(df_res.started_at)
+    df_res.started_at = df_res.started_at.dt.floor('d')
+    df_res = df_res.groupby(["started_at",
+                             "start_station_id"]).value_counts().reset_index()
+    df_res = df_res.rename(columns={"started_at": "date",
+                                    "start_station_id": "station_id",
+                                    0: "users_count"})
+    return df_res
 
 
 def update_month_data(main_df, month, year):
@@ -32,14 +47,19 @@ def update_month_data(main_df, month, year):
         month = f"0{month}"
     print(f"_____ Processing {month}/{year}... _____")
 
-    url = f'https://s3.amazonaws.com/tripdata/{year}{month}-citibike-tripdata.csv.zip'
+    if f"{year}{month}" in ["202206", "202207"]:
+        citibike = "citbike"
+    else:
+        citibike = "citibike"
+    url = f'https://s3.amazonaws.com/tripdata/{year}{month}-{citibike}-tripdata.csv.zip'
+    print(url)
     filename = "data/" + url.split('/')[-1].split(".")[0] + ".csv"
     fn_list = filename.split(".")
     processed_filename = fn_list[-2] + "_processed." + fn_list[-1]
 
     if os.path.isfile(processed_filename):
         print("Retrieving DataFrame from the existing csv file...💿")
-        return pd.concat([pd.read_csv(processed_filename, index_col=0), main_df])
+        return pd.concat([pd.read_csv(processed_filename), main_df])
 
     print('Downloading Started...⏳')
 
@@ -56,18 +76,9 @@ def update_month_data(main_df, month, year):
 
     original_df = pd.read_csv(filename)
 
-    # check if there is a file with station ids, names, coords mapping
-    if not os.path.isfile("data/stations_info.json"):
-        clipped_df = original_df[["start_station_id", "start_station_name",
-                                  "start_lat", "start_lng"]].drop_duplicates()
-        stations_info_dict = {
-            row[0]: {
-                "station_name": row[1],
-                "coords": row[2:]
-                } for row in clipped_df.itertuples(index=False)}
-
-        with open('data/stations_info.json', 'w') as fp:
-            json.dump(stations_info_dict, fp)
+    if not os.path.isfile("data/stations_info.csv"):
+        stations_info_df = select_stations_info(original_df)
+        stations_info_df.to_csv("data/stations_info.csv", index=False)
 
     processed_df = process_df(original_df)
 
@@ -80,25 +91,28 @@ def update_month_data(main_df, month, year):
     return pd.concat([processed_df, main_df])
 
 
-
 def get_citibike_data(start_date="04/2021", end_date="10/2022") -> pd.DataFrame:
 
     start_month, start_year = start_date.split("/")[0], start_date.split("/")[1]
     end_month, end_year = end_date.split("/")[0], end_date.split("/")[1]
 
-    res = pd.DataFrame(columns=["date", "station_id", "users_count"])
+    df_res = pd.DataFrame(columns=["date", "station_id", "users_count"])
 
     if start_year == end_year:
         for month in range(int(start_month), int(end_month) + 1):
-            res =  update_month_data(res, month, start_year)
+            df_res =  update_month_data(df_res, month, start_year)
 
     else:
         for month in range(int(start_month), 13):
-            res =  update_month_data(res, month, start_year)
+            df_res =  update_month_data(df_res, month, start_year)
         for month in range(1, int(end_month) + 1):
-            res =  update_month_data(res, month, end_year)
+            df_res =  update_month_data(df_res, month, end_year)
 
-    return res.reset_index(drop=True)
+    df_res["users_count"] = df_res["users_count"].astype(int)
+
+    print("\n✅ Done ✅")
+
+    return df_res.reset_index(drop=True)
 
 
 ################################################################################
@@ -134,22 +148,25 @@ def rate_of_change(df, window):
     M = df["users_count"].diff(window - 1)
     N = df["users_count"].shift(window - 1)
     df[f'rate_of_change_{window}_days'] = (M / N) * 100
+    df[f'rate_of_change_{window}_days'] = df[f'rate_of_change_{window}_days'].astype(float)
     return df
 
 
 def engineer_citibike_features(df):
-    res = df.copy()
-    res = moving_average(res, 7).dropna()
-    res = moving_average(res, 14).dropna()
-    res = moving_average(res, 56).dropna()
+    df_res = df.copy()
+    df_res['users_count_next_day'] = df_res.groupby('station_id')['users_count'].shift(-1)
+    df_res = moving_average(df_res, 7).dropna()
+    df_res = moving_average(df_res, 14).dropna()
+    df_res = moving_average(df_res, 56).dropna()
 
 
     for i in [7, 14, 56]:
         for func in [moving_std, exponential_moving_average,
                      exponential_moving_std, rate_of_change
                      ]:
-            res = func(res, i).dropna()
-    return res.reset_index(drop=True)
+            df_res = func(df_res, i).dropna()
+    df_res = df_res.reset_index(drop=True)
+    return df_res
 
 
 ###############################################################################
@@ -165,13 +182,68 @@ def get_weather_data(city, start_date, end_date):
     API_KEY = os.getenv("VISUALCROSSING_API_KEY")
     # API_KEY = ""
 
-    res = parse_weather_data(city, start_date, end_date, API_KEY)
+    df_res = parse_weather_data(city, start_date, end_date, API_KEY)
     # drop redundant columns
-    res = res.drop(columns=["name", "icon", "stations", "description",
-                            "sunrise", "sunset", "preciptype", "severerisk"])
-    # OneHotEncode 'conditions' feature
-    res = pd.get_dummies(res, columns=['conditions'])
+    df_res = df_res.drop(columns=["name", "icon", "stations", "description",
+                                  "sunrise", "sunset", "preciptype",
+                                  "severerisk", "conditions"])
 
-    res = res.rename(columns={"datetime": "date"})
+    df_res = df_res.rename(columns={"datetime": "date"})
 
-    return res
+    return df_res
+
+
+###############################################################################
+# Streamlit
+def decode_features(df, feature_view):
+    """Decodes features in the input DataFrame using corresponding Hopsworks Feature Store transformation functions"""
+    df_res = df.copy()
+
+    import inspect
+
+    print(feature_view)
+    td_transformation_functions = feature_view._batch_scoring_server._transformation_functions
+
+    res = {}
+    for feature_name in td_transformation_functions:
+        if feature_name in df_res.columns:
+            td_transformation_function = td_transformation_functions[feature_name]
+            sig, foobar_locals = inspect.signature(td_transformation_function.transformation_fn), locals()
+            param_dict = dict([(param.name, param.default) for param in sig.parameters.values() if param.default != inspect._empty])
+            if td_transformation_function.name == "min_max_scaler":
+                df_res[feature_name] = df_res[feature_name].map(
+                    lambda x: x * (param_dict["max_value"] - param_dict["min_value"]) + param_dict["min_value"])
+
+            elif td_transformation_function.name == "standard_scaler":
+                df_res[feature_name] = df_res[feature_name].map(
+                    lambda x: x * param_dict['std_dev'] + param_dict["mean"])
+            elif td_transformation_function.name == "label_encoder":
+                dictionary = param_dict['value_to_index']
+                dictionary_ = {v: k for k, v in dictionary.items()}
+                df_res[feature_name] = df_res[feature_name].map(
+                    lambda x: dictionary_[x])
+    return df_res
+
+
+def get_model():
+    # load our Model
+    import os
+    TARGET_FILE = "model.pkl"
+    list_of_files = [os.path.join(dirpath,filename) for dirpath, _, filenames in os.walk('.') for filename in filenames if filename == TARGET_FILE]
+
+    if list_of_files:
+        model_path = list_of_files[0]
+        model = joblib.load(model_path)
+    else:
+        if not os.path.exists(TARGET_FILE):
+            mr = project.get_model_registry()
+            EVALUATION_METRIC="r2_score"  # or r2_score
+            SORT_METRICS_BY="max"
+            # get best model based on custom metrics
+            model = mr.get_best_model("MLPRegressor",
+                                      EVALUATION_METRIC,
+                                      SORT_METRICS_BY)
+            model_dir = model.download()
+            model = joblib.load(model_dir + "/model.pkl")
+
+    return model
---
+++ advanced_tutorials/citibike/streamlit_app.py
@@ -0,0 +1,200 @@
+from datetime import timedelta, datetime
+from random import sample
+
+import pandas as pd
+import streamlit as st
+import hopsworks
+import plotly.express as px
+import altair as alt
+
+from functions import decode_features, get_model
+
+
+def fancy_header(text, font_size=24):
+    res = f'<span style="color:#ff5f27; font-size: {font_size}px;">{text}</span>'
+    st.markdown(res, unsafe_allow_html=True )
+
+
+st.title('🚲 Citibike Usage Prediction 🚲')
+
+
+st.write(36 * "-")
+fancy_header('\n📡 Connecting to Hopsworks Feature Store...')
+
+st.write("Logging... ")
+# (Attention! If the app has stopped at this step,
+# please enter your Hopsworks API Key in the commmand prompt.)
+project = hopsworks.login()
+fs = project.get_feature_store()
+
+
+@st.cache(allow_output_mutation=True)
+def get_feature_view():
+    feature_view = fs.get_feature_view(
+        name = 'citibike_feature_view',
+        version = 1
+    )
+
+    return feature_view
+
+
+st.write("Getting the Feature View...")
+feature_view = get_feature_view()
+st.write("Successfully connected!✔️")
+
+st.write(36 * "-")
+fancy_header('\n🪝 Retriving some useful data from Feature Store...')
+# I will to write @st.cache to cache retrieved data and
+# do not connect and download it every time
+@st.cache(allow_output_mutation=True)
+def get_data_from_feature_groups():
+    citibike_stations_info_fg = fs.get_or_create_feature_group(
+        name="citibike_stations_info",
+        version=1
+        )
+    stations_info_df = citibike_stations_info_fg.read()
+
+    meteorological_measurements_fg = fs.get_or_create_feature_group(
+        name="meteorological_measurements",
+        version=1
+        )
+    last_date_in_df = meteorological_measurements_fg.read()["date"].max()
+
+    return stations_info_df, last_date_in_df
+
+
+stations_info_df, last_date_in_df = get_data_from_feature_groups()
+
+st.write("✅ Done!")
+
+st.write(36 * "-")
+fancy_header('\n☁️ Getting batch data from Feature Store...')
+
+@st.cache(allow_output_mutation=True, suppress_st_warning=True)
+def get_batch_data(last_date_in_df):
+    start_date = datetime.strptime(last_date_in_df, "%Y-%m-%d") - timedelta(days=7)
+    start_time = int(start_date.timestamp()) * 1000
+    df_batch = feature_view.get_batch_data(start_time=start_time)
+
+    return df_batch
+
+
+df_batch = get_batch_data(last_date_in_df=last_date_in_df)
+st.write("✅ Done!")
+
+
+st.write(36 * "-")
+fancy_header('\n🪛 A little of Data Preprocessing...')
+table_df = decode_features(df_batch[["date", "users_count", "station_id"]],
+                           feature_view)
+
+# all_stations_list = ['6389.09', '6401.01', '6569.07', '6578.01', '6605.08', '6670.03',
+#                  '5238.05', '5303.08', '5484.09', '5539.06', '5561.06', '5752.07',
+#                  '5755.09', '6004.06', '6089.07', '6432.11', '6679.11', '6747.06',
+#                  '6873.01', '6923.20', '6960.10', '7003.03', '7023.04', '7059.08',
+#                  '7098.05', '7393.09', '7450.05', '7580.01', '5623.03', '5626.15',
+#                  '5659.05', '5755.14', '5788.13', '5847.01', '5905.12', '5938.11',
+#                  '5955.12', '5964.01', '6089.08', '6137.04', '6203.02', '6257.03',
+#                  '6306.01', '6422.09', '6450.12', '5820.08', '5838.09', '5847.08',
+#                  '5854.09', '5938.01', '5977.01', '6055.08', '6072.06', '6072.14',
+#                  '6098.10', '6115.06', '6140.05', '6206.08']
+
+st.write(36 * "-")
+# stations_list = sample(all_stations_list, 25)
+# idk why but streamlit doesnt work when I use code above
+stations_list = ['7450.05', '7580.01', '5623.03', '5626.15',
+                '5659.05', '5755.14', '5788.13', '5847.01', '5905.12', '5938.11',
+                '5955.12', '5964.01', '6089.08', '6137.04', '6203.02', '6257.03',
+                '6306.01', '6422.09', '6450.12', '5820.08', '5838.09', '5847.08',
+                '5854.09', '5938.01', '5977.01', '6055.08',]
+
+stations_info_dict_1 = stations_info_df.set_index("station_id").to_dict()
+stations_info_dict_2 = stations_info_df.set_index("station_name").to_dict()
+stations_list_names = list(map(lambda x: stations_info_dict_1["station_name"][x], stations_list))
+st.write("✅ Done!")
+
+fancy_header('\n🏙 Please select citibike stations to process...')
+with st.form("stations_selection"):
+   selected_stations_names = st.multiselect('Choose any number of stations.', stations_list_names)
+   # Every form must have a submit button.
+   submitted = st.form_submit_button("Submit")
+
+selected_stations = list(map(lambda x: stations_info_dict_2["station_id"][x], selected_stations_names))
+
+table_df = table_df[table_df.station_id.isin(selected_stations)]
+stations_info_df = stations_info_df[stations_info_df.station_id.isin(selected_stations)]
+
+
+st.write(36 * "-")
+fancy_header('\n🗺 Lets find selected stations on map...')
+def get_map(stations_info_df):
+    fig = px.scatter_mapbox(stations_info_df,
+                            lat="lat",
+                            lon="long",
+                            zoom=9.5,
+                            hover_name="station_name",
+                            height=400,
+                            width=600)
+
+    fig.update_layout(mapbox_style="open-street-map")
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    return fig
+
+st.plotly_chart(get_map(stations_info_df))
+
+st.write(36 * "-")
+fancy_header('\n 🤖 Getting the model...')
+model = get_model()
+st.write("✅ Done!")
+
+st.write(36 * "-")
+fancy_header('\n 🧠 Predicting...')
+df_for_model = df_batch.loc[table_df.index].drop(columns=["date", "station_id", "users_count_next_day"])
+
+model.predict(df_for_model)
+
+temp_df = pd.DataFrame()
+temp_df["users_count_next_day"] = model.predict(df_for_model)
+temp_df = decode_features(temp_df, feature_view)
+st.write("✅ Done!")
+
+st.write(36 * "-")
+fancy_header('\n📈 Plotting our results...')
+table_df["users_count_next_day"] = temp_df["users_count_next_day"].apply(lambda x:int(max(0, x))).values
+table_df = table_df.sort_values("date")
+
+table_date_max = table_df["date"].max()
+
+for station in selected_stations:
+    pred_value = table_df[(table_df.date == table_date_max) & (table_df.station_id == station)]\
+                         ["users_count_next_day"].tolist()[0]
+    new_row = {"date": "Tomorrow (Prediction)",
+               "users_count": pred_value,
+               "station_id": station,
+               "users_count_next_day": -1}
+    table_df = table_df.append(new_row, ignore_index=True)
+
+
+table_df.station_id = table_df.station_id.apply(lambda x: stations_info_dict_1["station_name"][x])
+table_df = table_df.rename(columns={"station_id": "Station address",
+                                    "users_count": "# of usecases",
+                                     "date": "Date"})
+
+alt.renderers.enable('altair_viewer')
+
+rect = alt.Chart(table_df).mark_rect().encode(alt.X('Station address'),
+                                              alt.Y('Date'),
+                                              alt.Color('# of usecases')
+                                             )
+
+text = rect.mark_text(baseline='middle').encode(text='# of usecases', color=alt.value('red'))
+
+layer = alt.layer(
+    rect, text
+)
+
+# layer.show()
+st.altair_chart(layer, use_container_width=True)
+
+st.subheader('\n🎉 📈 🤝 App Finished Successfully 🤝 📈 🎉')
+st.button("Re-run")
