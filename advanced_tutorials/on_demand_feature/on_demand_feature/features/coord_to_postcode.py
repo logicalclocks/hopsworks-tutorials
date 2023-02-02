66ca4b9a48e7a89ee23da44238116f19078172d2
davitbzh <davit.bzhalava@gmail.com>
2023-02-02 14:30:19 +0100

    ondemand_ft_computation

---
+++ advanced_tutorials/on_demand_feature/on_demand_feature/features/coord_to_postcode.py
@@ -0,0 +1,13 @@
+import geopy
+
+
+def coord2zipcode(x):
+    geolocator = geopy.Nominatim(user_agent="check_1")
+    location = geolocator.reverse("{}, {}".format(x['latitude'], x['longitude']))
+    if 'address' in location.raw:
+        if 'postcode' in location.raw['address']:
+            return int(location.raw['address']['postcode'])
+        else:
+            return 0
+    else:
+        return 0
