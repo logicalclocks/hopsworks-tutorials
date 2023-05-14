7b83d0e7b08e7352d6ac6334ddd5cc47d6fa9ccb
Maksym Zhytnikov <63515947+Maxxx-zh@users.noreply.github.com>
2023-05-14 23:25:57 +0300

    Update provision.sh

--- integrations/dbt_bq/provision.sh
+++ integrations/dbt_bq/provision.sh
@@ -1,16 +1,13 @@
-# Predefine variables
-CLUSTER_NAME='YOUR_CLUSTER_NAME'
-REGION='YOUR_REGION'
-ZONE='YOUR_ZONE'
-PROJECT='YOUR_PROJECT_NAME'
-BUCKET='YOUR_BUCKET_NAME'
+# Predefine a Cluster name, Region, and Project
+CLUSTER_NAME='dbt-hops'
+REGION='europe-central2'
+PROJECT='hops-20'
 
-# Dataproc cluster creation
+# Cluster creation
 gcloud dataproc clusters create $CLUSTER_NAME \
-    --bucket $BUCKET \
+    --bucket maksym_gcs_bucket \
     --region $REGION \
-    --zone $ZONE \
-    --project $PROJECT \
+    --zone europe-central2-c \
     --master-machine-type n1-standard-2 \
     --master-boot-disk-size 50GB \
     --num-workers 2 \
@@ -18,9 +15,9 @@ gcloud dataproc clusters create $CLUSTER_NAME \
     --worker-boot-disk-size 50GB \
     --image-version 2.0-debian10 \
     --optional-components JUPYTER \
-    --initialization-actions 'gs://goog-dataproc-initialization-actions-${REGION}/connectors/connectors.sh' \
-    --initialization-actions 'gs://goog-dataproc-initialization-actions-${REGION}/python/pip-install.sh' \
-    --metadata bigquery-connector-version=1.2.0 \
-    --metadata spark-bigquery-connector-version=0.21.0 \
-    --metadata 'PIP_PACKAGES=hopsworks==3.1.0rc0' \
-    --properties spark:spark.jars=gs://spark-lib/bigquery/spark-bigquery-with-dependencies_2.12-0.23.2.jar
+    --project $PROJECT \
+    --initialization-actions 'gs://goog-dataproc-initialization-actions-europe-central2/connectors/connectors.sh' \
+    --metadata 'PIP_PACKAGES=hopsworks==3.2.0rc0' \
+    --initialization-actions 'gs://maksym_gcs_bucket/pip-install.sh' \
+    --metadata truststore-uri=gs://maksym_gcs_bucket/client/trustStore.jks \
+    --properties=^#^spark:spark.jars=gs://spark-lib/bigquery/spark-bigquery-with-dependencies_2.12-0.23.2.jar,gs://maksym_gcs_bucket/client/hudi-spark3-bundle_2.12-0.10.0.3.jar,gs://maksym_gcs_bucket/client/hopsfs-client-3.2.0.5-EE-SNAPSHOT.jar,gs://maksym_gcs_bucket/client/hopsfs-client-api-3.2.0.5-EE-SNAPSHOT.jar,gs://maksym_gcs_bucket/client/hsfs-spark-3.2.0-RC0.jar,gs://maksym_gcs_bucket/client/spark-avro_2.12-3.1.1.3.jar,gs://maksym_gcs_bucket/client/spark-sql-kafka-0-10_2.12-3.1.1.3.jar,gs://maksym_gcs_bucket/client/hudi-utilities-bundle_2.12-0.10.0.3.jar
