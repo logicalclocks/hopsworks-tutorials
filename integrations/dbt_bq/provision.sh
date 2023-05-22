45eec18a8ad188d6973e94e70e78bc485fbff307
Maksym Zhytnikov <63515947+Maxxx-zh@users.noreply.github.com>
2023-05-22 17:19:49 +0300

    Update provision.sh

--- integrations/dbt_bq/provision.sh
+++ integrations/dbt_bq/provision.sh
@@ -1,11 +1,12 @@
 # Predefine a Cluster name, Region, and Project
-CLUSTER_NAME='dbt-hops'
-REGION='europe-central2'
-PROJECT='hops-20'
+CLUSTER_NAME='YOUR_CLUSTER_NAME'
+REGION='YOUR_REGION'
+PROJECT='YOUR_PROJECT_NAME'
+BUCKET_NAME='YOUR_BUCKET_NAME'
 
 # Cluster creation
 gcloud dataproc clusters create $CLUSTER_NAME \
-    --bucket maksym_gcs_bucket \
+    --bucket $BUCKET_NAME \
     --region $REGION \
     --zone europe-central2-c \
     --master-machine-type n1-standard-2 \
@@ -17,7 +18,5 @@ gcloud dataproc clusters create $CLUSTER_NAME \
     --optional-components JUPYTER \
     --project $PROJECT \
     --initialization-actions 'gs://goog-dataproc-initialization-actions-europe-central2/connectors/connectors.sh' \
-    --metadata 'PIP_PACKAGES=hopsworks==3.2.0rc0' \
-    --initialization-actions 'gs://maksym_gcs_bucket/pip-install.sh' \
-    --metadata truststore-uri=gs://maksym_gcs_bucket/client/trustStore.jks \
-    --properties=^#^spark:spark.jars=gs://spark-lib/bigquery/spark-bigquery-with-dependencies_2.12-0.23.2.jar,gs://maksym_gcs_bucket/client/hudi-spark3-bundle_2.12-0.10.0.3.jar,gs://maksym_gcs_bucket/client/hopsfs-client-3.2.0.5-EE-SNAPSHOT.jar,gs://maksym_gcs_bucket/client/hopsfs-client-api-3.2.0.5-EE-SNAPSHOT.jar,gs://maksym_gcs_bucket/client/hsfs-spark-3.2.0-RC0.jar,gs://maksym_gcs_bucket/client/spark-avro_2.12-3.1.1.3.jar,gs://maksym_gcs_bucket/client/spark-sql-kafka-0-10_2.12-3.1.1.3.jar,gs://maksym_gcs_bucket/client/hudi-utilities-bundle_2.12-0.10.0.3.jar
+    --initialization-actions=gs://$BUCKET_NAME/client/configureSpark.sh \
+    --properties=^#^spark:spark.jars=gs://spark-lib/bigquery/spark-bigquery-with-dependencies_2.12-0.23.2.jar,gs://$BUCKET_NAME/client/hopsfs-client-3.2.0.5-EE-SNAPSHOT.jar,gs://$BUCKET_NAME/client/hopsfs-client-api-3.2.0.5-EE-SNAPSHOT.jar,gs://$BUCKET_NAME/client/hudi-spark3-bundle_2.12-0.10.0.3.jar,gs://$BUCKET_NAME/client/hsfs-spark-3.3.0-RC0.jar#spark:spark.jars.packages=org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.3,org.apache.spark:spark-avro_2.12:3.1.3
