dc732bfa451bb0363d9c1ed0ad2d83bd0f5291ff
davitbzh <davit.bzhalava@gmail.com>
2023-06-07 22:45:23 +0200

    flink-1.17

--- integrations/java/flink/src/main/java/com/hopsworks/tutorials/flink/utils/Utils.java
+++ integrations/java/flink/src/main/java/com/hopsworks/tutorials/flink/utils/Utils.java
@@ -1,14 +1,17 @@
 package com.hopsworks.tutorials.flink.utils;
 
 import com.logicalclocks.hsfs.FeatureStoreException;
+import com.logicalclocks.hsfs.flink.HopsworksConnection;
 import com.logicalclocks.hsfs.metadata.HopsworksClient;
 import com.logicalclocks.hsfs.metadata.HopsworksHttpClient;
 
+import java.io.IOException;
 import java.util.Properties;
 
 public class Utils {
   
-  public Properties getKafkaProperties(String topic) throws FeatureStoreException {
+  public Properties getKafkaProperties() throws FeatureStoreException, IOException {
+    HopsworksConnection connection = HopsworksConnection.builder().build();
     HopsworksHttpClient client = HopsworksClient.getInstance().getHopsworksHttpClient();
     Properties properties = new Properties();
     properties.put("bootstrap.servers", "broker.kafka.service.consul:9091");
@@ -19,21 +22,13 @@ public class Utils {
     properties.put("ssl.keystore.password", client.getCertKey());
     properties.put("ssl.key.password", client.getCertKey());
     properties.put("ssl.endpoint.identification.algorithm", "");
-    properties.put("topic", topic);
+    properties.put("enable.idempotence", false);
     return properties;
   }
   
-  public Properties getKafkaProperties() throws FeatureStoreException {
-    HopsworksHttpClient client = HopsworksClient.getInstance().getHopsworksHttpClient();
-    Properties properties = new Properties();
-    properties.put("bootstrap.servers", "broker.kafka.service.consul:9091");
-    properties.put("security.protocol", "SSL");
-    properties.put("ssl.truststore.location", client.getTrustStorePath());
-    properties.put("ssl.truststore.password", client.getCertKey());
-    properties.put("ssl.keystore.location", client.getKeyStorePath());
-    properties.put("ssl.keystore.password", client.getCertKey());
-    properties.put("ssl.key.password", client.getCertKey());
-    properties.put("ssl.endpoint.identification.algorithm", "");
+  public Properties getKafkaProperties(String topic) throws FeatureStoreException, IOException {
+    Properties properties = getKafkaProperties();
+    properties.put("topic", topic);
     return properties;
   }
 }
