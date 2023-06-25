062442f47b5665095ec0c7aa88ffbb1df27653e6
Maksym Zhytnikov <63515947+Maxxx-zh@users.noreply.github.com>
2023-06-26 00:40:18 +0300

    DBT with External FG

---
+++ integrations/dbt_bq/models/example/big_query_insert.py
@@ -0,0 +1,26 @@
+# Imports
+import hsfs
+
+def model(dbt, session):
+    # Setup cluster usage
+    dbt.config(
+        submission_method="cluster",
+        dataproc_cluster_name="{YOUR_DATAPROC_CLUSTER_NAME}",
+    )
+
+    # Read data_pipeline Python model
+    data_pipeline = dbt.ref("data_pipeline")
+
+    # Define the list of columns to drop
+    columns_to_drop = ['index_column', 'hour', 'day', 'temperature_diff', 'wind_speed_category']
+
+    # Drop the specified columns
+    data_pipeline = data_pipeline.drop(*columns_to_drop)
+
+    # Write data to BigQuery table
+    data_pipeline.write.format('bigquery') \
+        .option('table', 'weather_data_demo.weather_data_demo_table') \
+        .mode('append') \
+        .save()
+
+    return data_pipeline
