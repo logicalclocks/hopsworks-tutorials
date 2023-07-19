a97019f516c2d1f532f32b920b8ded9d1239499d
davitbzh <44586065+davitbzh@users.noreply.github.com>
2023-07-19 23:45:51 +0200

    [FSTORE-888] Upgrade to flink 1.17 (#174)

--- integrations/java/flink/src/main/java/com/hopsworks/tutorials/flink/utils/Utils.java
+++ integrations/java/flink/src/main/java/com/hopsworks/tutorials/flink/utils/Utils.java
@@ -10,22 +10,6 @@ import java.util.Properties;
 
 public class Utils {
   
-  public Properties getKafkaProperties(String topic) throws FeatureStoreException, IOException {
-    HopsworksConnection connection = HopsworksConnection.builder().build();
-    HopsworksHttpClient client = HopsworksClient.getInstance().getHopsworksHttpClient();
-    Properties properties = new Properties();
-    properties.put("bootstrap.servers", "broker.kafka.service.consul:9091");
-    properties.put("security.protocol", "SSL");
-    properties.put("ssl.truststore.location", client.getTrustStorePath());
-    properties.put("ssl.truststore.password", client.getCertKey());
-    properties.put("ssl.keystore.location", client.getKeyStorePath());
-    properties.put("ssl.keystore.password", client.getCertKey());
-    properties.put("ssl.key.password", client.getCertKey());
-    properties.put("ssl.endpoint.identification.algorithm", "");
-    properties.put("topic", topic);
-    return properties;
-  }
-  
   public Properties getKafkaProperties() throws FeatureStoreException, IOException {
     HopsworksConnection connection = HopsworksConnection.builder().build();
     HopsworksHttpClient client = HopsworksClient.getInstance().getHopsworksHttpClient();
@@ -38,6 +22,13 @@ public class Utils {
     properties.put("ssl.keystore.password", client.getCertKey());
     properties.put("ssl.key.password", client.getCertKey());
     properties.put("ssl.endpoint.identification.algorithm", "");
+    properties.put("enable.idempotence", false);
+    return properties;
+  }
+  
+  public Properties getKafkaProperties(String topic) throws FeatureStoreException, IOException {
+    Properties properties = getKafkaProperties();
+    properties.put("topic", topic);
     return properties;
   }
 }
