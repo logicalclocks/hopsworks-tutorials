eab86a47c015cacf4a30e08f70c9ec23f68a11c5
Maksym Zhytnikov <63515947+Maxxx-zh@users.noreply.github.com>
2023-07-26 21:09:54 +0300

    Explained every step of the tutorial

--- integrations/custom_transformation_function/transformations.py
+++ integrations/custom_transformation_function/transformations.py
@@ -10,8 +10,17 @@ def encode_city_name(city_name):
         int:
             The encoded city name as an integer.
     """
-    label_map = {'Madrid': 0, 'Sevilla': 1, 'Napoli': 2}
-    return label_map.get(city_name, -1)  # Return -1 if city_name is not found in label_map
+    # Define a mapping of city names to their corresponding integer labels
+    label_map = {
+        'Amsterdam': 0, 'Athina': 1, 'Berlin': 2, 'Gdansk': 3, 'Kraków': 4,
+        'London': 5, 'Madrid': 6, 'Marseille': 7, 'Milano': 8, 'München': 9,
+        'Napoli': 10, 'Paris': 11, 'Sevilla': 12, 'Stockholm': 13, 'Tallinn': 14,
+        'Varna': 15, 'Wien': 16
+    }
+    
+    # Return the integer label for the input city_name using label_map.get()
+    # If the city_name is not found in label_map, return -1 as the default value
+    return label_map.get(city_name, -1)
 
 
 def scale_pm2_5(pm2_5_value):
@@ -26,8 +35,14 @@ def scale_pm2_5(pm2_5_value):
         float:
             The scaled 'pm2_5' value.
     """
-    mean = 14.5  # Mean value of the sample 'pm2_5' column
-    std = 13  # Standard deviation
+    # Define the mean value of the 'pm2_5' column for the scaling process
+    mean = 14.5
 
+    # Define the standard deviation value of the 'pm2_5' column for the scaling process
+    std = 13
+
+    # Calculate the scaled 'pm2_5' value using the custom scaling formula
     scaled_pm2_5 = (pm2_5_value - mean) / std
+
+    # Return the scaled 'pm2_5' value
     return scaled_pm2_5
