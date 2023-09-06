ee59a254331c4d63e79554ff905573dc330cb2ab
davitbzh <davit.bzhalava@gmail.com>
2023-09-06 23:56:26 +0200

    java tutorial

---
+++ java/README.md
@@ -0,0 +1,43 @@
+# Online Feature Vector Retrieval Using Java Application
+
+## Introduction
+In this tutorial you will learn how to fetch feature vectors from online feature store for near real-time model serving
+using external java application. 
+
+
+
+## Clone tutorials repository
+This section requires maven; java 1.8 and git.
+
+```bash
+git clone https://github.com/logicalclocks/hopsworks-tutorials
+cd ./hopsworks-tutorials/java
+mvn clean package
+```
+
+## Create Feature Group and Feature View
+This tutorial comes with pyspark program with a code to create feature group and feature view:
+- `./setup/create_fg_fv.py`
+
+Feature group data is generated using `dbldatagen` library. 
+
+You can execute this pyspark program directly on Hopsworks cluster. Follow the documentation how to set up and run 
+[spark jobs](https://docs.hopsworks.ai/hopsworks-api/3.3/generated/api/jobs/)
+
+## Execute java application:
+Now you will create [connection](https://docs.hopsworks.ai/hopsworks-api/3.3/generated/api/connection/) with
+your Hopsworks cluster. For this you need to have Hopsworks cluster host address and [api key](https://docs.hopsworks.ai/3.3/user_guides/projects/api_key/create_api_key/)
+
+Then define environment variables 
+
+```bash
+HOPSWORKS_HOST=REPLACE_WITH_YOUR_HOPSWORKS_CLUSTER_HOST
+HOPSWORKS_API_KEY=REPLACE_WITH_YOUR_HOPSWORKS_API_KEY
+HOPSWORKS_PROJECT_NAME=REPLACE_WITH_YOUR_HOPSWORKS_PROJECT_NAME
+export FEATURE_VIEW_NAME=products_fv
+export FEATURE_VIEW_VERSION=1
+```
+
+```bash
+java -jar ./target/hopsworks-java-tutorial-3.4.0-SNAPSHOT-jar-with-dependencies.jar $HOPSWORKS_HOST $HOPSWORKS_API_KEY $HOPSWORKS_PROJECT_NAME $FEATURE_VIEW_NAME $FEATURE_VIEW_VERSION
+```
---
+++ java/src/main/java/com/hopsworks/tutorials/FeatureVectorBenchMarks.java
@@ -0,0 +1,66 @@
+package com.hopsworks.tutorials;
+
+import com.logicalclocks.hsfs.FeatureStore;
+import com.logicalclocks.hsfs.FeatureView;
+import com.logicalclocks.hsfs.HopsworksConnection;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class FeatureVectorBenchMarks {
+  
+  public static void main(String[] args) throws Exception {
+  
+    String host = args[0];
+    String projectName = args[1];
+    String apiKey = args[2];
+    String fvName = args[3];
+    Integer fvVersion = Integer.parseInt(args[4]);
+  
+    FeatureStore fs = HopsworksConnection.builder()
+      .host(host)
+      .project(projectName)
+      .apiKeyValue(apiKey)
+      .hostnameVerification(false)
+      .build()
+      .getFeatureStore();
+    
+    // get feature view
+    FeatureView fv = fs.getFeatureView(fvName, fvVersion);
+  
+    // single lookup sering vector
+    List<Object> singleVector = fv.getFeatureVector(new HashMap<String, Object>() {{
+      put("product_id", productIdGenerator());
+    }});
+    System.out.println("Feature values from single vector lookup");
+    singleVector.stream().forEach(System.out::println);
+  
+    // batch lookup sering vector
+    fv.initServing(true, true);
+    List<List<Object>> batchVector = fv.getFeatureVectors(productIdGenerator(160));
+    
+    // print results
+    for (List<Object> vector: batchVector) {
+      System.out.println("Feature values from batch lookup");
+      vector.stream().forEach(System.out::println);
+    }
+  }
+  
+  private static int productIdGenerator() {
+    int leftLimit = 0;
+    int rightLimit = 1000;
+    return leftLimit + (int) (Math.random() * (rightLimit - leftLimit));
+  }
+  
+  private static java.util.Map<java.lang.String,java.util.List<java.lang.Object>> productIdGenerator(int batch) {
+    List<Object> productIds = new ArrayList<>();
+    while (productIds.size() <= batch){
+      int productId = productIdGenerator();
+      if (!productIds.contains(productId)) {
+        productIds.add(productId);
+      }
+    }
+    return new HashMap<String, List<Object>>() {{put("product_id", productIds);}};
+  }
+}
