60e87a8b141babe1017093f3c103d3620baffff0
Maksym Zhytnikov <63515947+Maxxx-zh@users.noreply.github.com>
2023-11-22 23:30:55 +0200

    LoanApprovalP1

--- loan_approval/features/applicants.py
+++ loan_approval/features/applicants.py
@@ -2,13 +2,31 @@ import pandas as pd
 import datetime
 import numpy as np
 
-def home_ownership(home_ownership: str)-> str:
-    if (home_ownership == 'ANY' or home_ownership == 'NONE') :
+def home_ownership(home_ownership: str) -> str:
+    """
+    Transform the home ownership status.
+
+    Parameters:
+    - home_ownership (str): The original home ownership status.
+
+    Returns:
+    - str: Transformed home ownership status.
+    """
+    if (home_ownership == 'ANY' or home_ownership == 'NONE'):
         return 'OTHER'
     return home_ownership
 
 
 def pub_rec(number):
+    """
+    Transform the public record number.
+
+    Parameters:
+    - number: The original public record number.
+
+    Returns:
+    - int: Transformed public record number (0 or 1).
+    """
     if number == 0.0:
         return 0
     else:
@@ -16,6 +34,15 @@ def pub_rec(number):
 
 
 def mort_acc(number):
+    """
+    Transform the mortgage account number.
+
+    Parameters:
+    - number: The original mortgage account number.
+
+    Returns:
+    - int: Transformed mortgage account number (0, 1, or the original number).
+    """
     if number == 0.0:
         return 0
     elif number >= 1.0:
@@ -25,6 +52,15 @@ def mort_acc(number):
 
 
 def pub_rec_bankruptcies(number):
+    """
+    Transform the public record bankruptcies number.
+
+    Parameters:
+    - number: The original public record bankruptcies number.
+
+    Returns:
+    - int: Transformed public record bankruptcies number (0, 1, or the original number).
+    """
     if number == 0.0:
         return 0
     elif number >= 1.0:
@@ -34,6 +70,17 @@ def pub_rec_bankruptcies(number):
 
 
 def fill_mort_acc(total_acc, mort_acc, total_acc_avg):
+    """
+    Fill missing values in mortgage account based on total account average.
+
+    Parameters:
+    - total_acc: Total account number.
+    - mort_acc: Original mortgage account number.
+    - total_acc_avg: Average mortgage account number based on total account.
+
+    Returns:
+    - float: Filled mortgage account number.
+    """
     if np.isnan(mort_acc):
         return total_acc_avg[total_acc].round()
     else:
@@ -41,9 +88,26 @@ def fill_mort_acc(total_acc, mort_acc, total_acc_avg):
 
 
 def earliest_cr_line(earliest_cr_line):
+    """
+    Transform the earliest credit line date to the year.
+
+    Parameters:
+    - earliest_cr_line: The original earliest credit line date.
+
+    Returns:
+    - int: The year of the earliest credit line.
+    """
     return earliest_cr_line.year
 
 
-def mean_mort_acc(applicants_df: pd.DataFrame)->pd.DataFrame:
-    return applicants_df.groupby(by='total_acc').mean().mort_acc
+def mean_mort_acc(applicants_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Calculate the mean mortgage account number grouped by total account.
 
+    Parameters:
+    - applicants_df (pd.DataFrame): DataFrame containing applicant data.
+
+    Returns:
+    - pd.DataFrame: DataFrame with the mean mortgage account number.
+    """
+    return applicants_df.groupby(by='total_acc').mean().mort_acc
--- loan_approval/features/loans.py
+++ loan_approval/features/loans.py
@@ -2,10 +2,25 @@ import pandas as pd
 import datetime
 import numpy as np
 
-# On-Demand feature function
 def zipcode(address):
-    zip_code=int(address)
-    l=len(str(zip_code))
-    if l==5:
+    """
+    Transform the address to a standardized zip code.
+
+    Parameters:
+    - address (str or int): The original zip code.
+
+    Returns:
+    - str: Standardized zip code (5 digits) or "0" if not a valid zip code.
+    """
+    # Convert the address to an integer
+    zip_code = int(address)
+
+    # Determine the length of the zip code
+    l = len(str(zip_code))
+
+    # Check if the length is 5, return the zip code as a string
+    if l == 5:
         return str(zip_code)
+
+    # Return "0" for invalid zip codes
     return "0"
