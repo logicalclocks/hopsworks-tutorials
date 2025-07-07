40f6e0f47c336f00a82c9a5a5852e2df328b8e20
manu-sj <152865565+manu-sj@users.noreply.github.com>
2025-07-07 14:55:36 +0200

    Updating links in the 4.3 branch (#332)

--- integrations/mage_ai/mage_tutorial/data_loaders/profiles.py
+++ integrations/mage_ai/mage_tutorial/data_loaders/profiles.py
@@ -1,27 +1,28 @@
 import io
 import pandas as pd
 import requests
-if 'data_loader' not in globals():
+
+if "data_loader" not in globals():
     from mage_ai.data_preparation.decorators import data_loader
-if 'test' not in globals():
+if "test" not in globals():
     from mage_ai.data_preparation.decorators import test
 
 
 @data_loader
 def load_profiles_data(*args, **kwargs):
     # Specify the URL for the data
-    url = "https://repo.hops.works/master/hopsworks-tutorials/data/card_fraud_data/"
+    url = "https://repo.hops.works/branch-4.3/hopsworks-tutorials/data/card_fraud_data/"
 
     # Read the 'profiles.csv' file
     # Parse the 'birthdate' column as dates
     profiles_df = pd.read_csv(
-        url + "profiles.csv", 
+        url + "profiles.csv",
         parse_dates=["birthdate"],
-        )
+    )
 
     return profiles_df
 
 
 @test
 def test_output(output, *args) -> None:
-    assert output is not None, 'The output is undefined'
+    assert output is not None, "The output is undefined"
